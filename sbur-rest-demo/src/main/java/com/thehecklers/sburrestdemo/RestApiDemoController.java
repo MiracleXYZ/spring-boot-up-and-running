28b7ac063cc2dec907576b1840c0bc5088ea98be
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-08-08 22:13:14 +0800

    getCoffeeById

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
@@ -2,8 +2,10 @@ package com.thehecklers.sburrestdemo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,16 @@ public class RestApiDemoController {
     @GetMapping("/coffees")
     Iterable<Coffee> getCoffees() {
         return coffees;
+    }
+
+    @GetMapping("/coffees/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        for (Coffee c : coffees) {
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
+
+        return Optional.empty();
     }
 }
