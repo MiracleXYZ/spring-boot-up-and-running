2434add8f6c568df34aae7742ba04a73949cd807
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-08-08 22:01:15 +0800

    I'd better place the classes into different files...

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -1,7 +1,5 @@
 package com.thehecklers.sburrestdemo;
 
-import java.util.UUID;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,30 +10,4 @@ public class SburRestDemoApplication {
 		SpringApplication.run(SburRestDemoApplication.class, args);
 	}
 
-}
-
-class Coffee {
-	private final String id;
-	private String name;
-
-	public Coffee(String id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public Coffee(String name) {
-		this(UUID.randomUUID().toString(), name);
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
