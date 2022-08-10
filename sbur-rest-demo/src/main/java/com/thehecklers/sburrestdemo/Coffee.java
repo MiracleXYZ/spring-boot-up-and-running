936b22c1294d71de03389f4674fc8ed9099308ac
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-08-10 23:28:00 +0800

    @Entity

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/Coffee.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/Coffee.java
@@ -2,9 +2,17 @@ package com.thehecklers.sburrestdemo;
 
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Coffee {
-    private final String id;
+    @Id
+    private String id;
     private String name;
+
+    public Coffee() {
+    }
 
     public Coffee(String id, String name) {
         this.id = id;
@@ -17,6 +25,10 @@ public class Coffee {
 
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
