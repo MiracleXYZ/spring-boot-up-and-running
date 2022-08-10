458db2ec5556026b3a2375c7a0d2c749491cf81e
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-08-10 23:55:30 +0800

    A bit of polishing.

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
@@ -1,7 +1,5 @@
 package com.thehecklers.sburrestdemo;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -22,12 +20,6 @@ public class RestApiDemoController {
 
     public RestApiDemoController(CoffeeRepository coffeeRepository) {
         this.coffeeRepository = coffeeRepository;
-
-        this.coffeeRepository.saveAll(List.of(
-                new Coffee("Café Cereza"),
-                new Coffee("Café Ganador"),
-                new Coffee("Café Lareño"),
-                new Coffee("Café Três Pontas")));
     }
 
     @GetMapping
@@ -47,9 +39,9 @@ public class RestApiDemoController {
 
     @PutMapping("/{id}")
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        return (!coffeeRepository.existsById(id))
-                ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
-                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
+        return (coffeeRepository.existsById(id))
+                ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
