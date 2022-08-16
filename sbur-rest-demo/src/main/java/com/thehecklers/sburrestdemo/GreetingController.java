98c87ac91278e753d80338444c6bdd6484a1e32c
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-08-17 00:34:27 +0800

    ConfigurationProperties

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/GreetingController.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/GreetingController.java
@@ -1,6 +1,5 @@
 package com.thehecklers.sburrestdemo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,19 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    @Value("${greeting-name:Mirage}")
-    private String name;
+    private final Greeting greeting;
 
-    @Value("${greeting-coffee:${greeting-name} is drinking Cafe Ganador}")
-    private String coffee;
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
 
     @GetMapping
     String getGreeting() {
-        return name;
+        return greeting.getName();
     }
 
     @GetMapping("/coffee")
     String getNameAndCoffee() {
-        return coffee;
+        return greeting.getCoffee();
     }
 }
